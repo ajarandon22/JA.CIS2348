c3c76480ad992a976e76515240b6fdadd7f204c4
Jarandon Adams <ajarandon1@gmail.com>
2021-02-10 12:27:21 -0600

    Completed

--- Homework1/Zylabs1.12.py
+++ Homework1/Zylabs1.12.py
@@ -1,1 +1,11 @@
 # Jarandon Adams - 1812590
+
+# Create a program that will calculate the squared value of an Integer
+
+print("Squared Integer Calculator")
+
+print("Enter desired Integer ↓")
+userNum = int(input('Number:'))
+userNumSquared = userNum * userNum
+
+print(userNumSquared)

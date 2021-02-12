a31913b9e421511199a8132553b333ef547bbc7f
Jarandon Adams <ajarandon1@gmail.com>
2021-02-12 15:01:24 -0600

    Completed

--- Homework1/Zylabs3.18.py
+++ Homework1/Zylabs3.18.py
@@ -1,22 +1,22 @@
 # Jarandon Adams - 1812590
-import math
-
-wallheight = float(input('Enter wall height (feet):\n'))
-wallwidth = float(input('Enter wall width (feet):\n'))
-
-area = wallwidth * wallheight
-paintneeded = area / 350
-paintcansneeded = math.ceil(paintneeded)
-
-print("Wall area:", '{:.2f}'.format(area), "square feet")
-print("Paint needed:", '{:.2f}'.format(paintneeded), "gallons")
-print("Cans needed:", '{:.2f}'.format(paintcansneeded), "can(s)\n")
 
 color_dict = {
     'red': 35,
     'blue': 25,
     'green': 23
 }
+wallheight = float(input('Enter wall height (feet):\n'))
+wallwidth = float(input('Enter wall width (feet):\n'))
+
+area = wallwidth * wallheight
+paintneeded = area / 350
+paintcans = round(paintneeded)
+
+print("Wall area:", round(area), "square feet")
+print("Paint needed:", '{:.2f}'.format(paintneeded), "gallons")
+print("Cans needed:", paintcans, "can(s)\n")
+
 paintcolor = input("Choose a color to paint the wall:\n")
-print("Cost of purchasing", paintcolor, "paint: $" + str(color_dict[paintcolor]))
+
+print("Cost of purchasing", paintcolor, "paint: $" + str(paintcans*color_dict[paintcolor]))
 

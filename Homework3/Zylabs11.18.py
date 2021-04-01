df5132352964adbf9b3a5758e1ce3d517d61bdd4
Jarandon Adams <ajarandon1@gmail.com>
2021-03-31 22:48:45 -0500

    Completed

--- Homework3/Zylabs11.18.py
+++ Homework3/Zylabs11.18.py
@@ -1,1 +1,15 @@
 # Jarandon Adams - 1812590
+
+numbers_list = input().split()
+Output = []
+
+for num in numbers_list:
+    num = int(num)
+
+    if num > 0:
+        Output.append(num)
+
+Output.sort()
+
+for num in Output:
+    print(num, end=' ')
--- Homework3/Zylabs11.22.py
+++ Homework3/Zylabs11.22.py
@@ -1,1 +1,7 @@
 # Jarandon Adams - 1812590
+
+character_list = input()
+splits = character_list.split(" ")
+
+for x in splits:
+    print(x, splits.count(x))
